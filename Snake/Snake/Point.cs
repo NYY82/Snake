5a96e9e54d84adaa62317078edc519497c01172a
nyy <nyy82@yandex.ru>
2019-05-12 01:41:40 +0300

    Move

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -54,6 +54,11 @@ namespace Snake
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
         public override string ToString()
         {
             return x + ", " + y + ", " + sym;
