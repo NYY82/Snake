19a997ee36afb2bc6a7498c8600f703c34867372
nyy <nyy82@yandex.ru>
2019-05-12 16:44:30 +0300

    Обрабатываем столкновения

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -6,24 +6,16 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine : Figure 
+    class HorizontalLine : Figure
     {
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
-            for(int x = xLeft; x <= xRight; x++)
+            for (int x = xLeft; x <= xRight; x++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-        }
-        public override void Draw()
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            base.Draw();
-
-            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
